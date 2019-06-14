5dad9de60b84b4788535a01da4e0c9af8b863498
chenshuaiyu <1961569938@qq.com>
2019-06-14 19:06:07 +0800

    19.6.14 19:06

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,6 @@ module.exports = {
 	title: "chen's blog",
 	description: '个人博客',
 	themeConfig: {
-		search: false,
 		sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
 		lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
 		nav: [{
